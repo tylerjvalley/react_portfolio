0b153f097585be102be3a34065fc63fe765069bf
Tyler Valley <tvalley93@gmail.com>
2019-05-23 15:24:05 -0500

    Add tabs to project page

--- src/Containers/Projects/Projects.js
+++ src/Containers/Projects/Projects.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Navigation from '../../Components/Navigation/Navigation';
 import Aux from '../../HOC/Aux';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 import './Projects.css';
 
 class Projects extends Component {
@@ -14,7 +16,17 @@ class Projects extends Component {
             <div className="projects-page">
                 <Navigation />
                 <div className="category-tabs">
-                    <h1>Test</h1>
+                    <Tabs defaultActiveKey="react" id="uncontrolled-tab-example">
+                        <Tab eventKey="react" title="ReactJs">
+                            <h1>React Projects</h1>
+                        </Tab>
+                        <Tab eventKey="ruby" title="Ruby">
+                            <h1>Ruby Projects</h1>
+                        </Tab>
+                        <Tab eventKey="js" title="Vanilla JS">
+                            <h1>Javascript Projects</h1>
+                        </Tab>
+                    </Tabs>;
                 </div>
             </div>
         )
