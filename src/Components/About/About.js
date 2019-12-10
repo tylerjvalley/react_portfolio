389f88f8de2647af9ce789471cae33944ecbbf5f
Tyler Valley <tvalley93@gmail.com>
2019-12-10 15:59:26 -0600

    Minor css edit in about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -12,86 +12,86 @@ import NodeImage from '../../Images/nodejs.png';
 import './About.css';
 
 const aboutMe = () => (
-    <div className="about-page">
-        <Jumbotron className="about-me-jumbotron" fluid>
-            <Container>
-                <h1>About Me</h1>
-            </Container>
-        </Jumbotron>
+        <>
+            <Jumbotron className="about-me-jumbotron" fluid>
+                <Container>
+                    <h1>About Me</h1>
+                </Container>
+            </Jumbotron>
 
-        <div className="middle">
-            <div className="frontend-fullstack">
-                <h6>Front End focused with experience in full stack development</h6>
-            </div> 
+            <div className="middle">
+                <div className="frontend-fullstack">
+                    <h6>Front End focused with experience in full stack development</h6>
+                </div> 
 
-            
-            <Container className="icons">
-                <Row>
-                    <Col xs={4} md={4}>
-                        <Image src={JSImage} roundedCircle />
-                    </Col>
-                    <Col xs={4} md={4}>
-                        <Image src={ReactImage} roundedCircle />
-                    </Col>
-                    <Col xs={4} md={4}>
-                        <Image src={NodeImage} roundedCircle />
-                    </Col>
-                </Row>
-            </Container>
-            
-        </div>
+                
+                <Container className="icons">
+                    <Row>
+                        <Col xs={4} md={4}>
+                            <Image src={JSImage} roundedCircle />
+                        </Col>
+                        <Col xs={4} md={4}>
+                            <Image src={ReactImage} roundedCircle />
+                        </Col>
+                        <Col xs={4} md={4}>
+                            <Image src={NodeImage} roundedCircle />
+                        </Col>
+                    </Row>
+                </Container>
+                
+            </div>
 
-        <div className="info">
-            <div>
-                <h4>Motivated to produce results</h4>
-                <p>I always make sure that the projects I do, and the results that I have are exactly what I need them to be.  I give 100% to my assignments, and I love chipping away at problems until I end up with a great solution.</p>
+            <div className="info">
+                <div>
+                    <h4>Motivated to produce results</h4>
+                    <p>I always make sure that the projects I do, and the results that I have are exactly what I need them to be.  I give 100% to my assignments, and I love chipping away at problems until I end up with a great solution.</p>
+                </div>
+                <div>
+                    <h4>Focus on JavaScript</h4>
+                    <p>Over the past 4 years I've been working with JavaScript to create beautiful websites and projects for myself and for clients.  I love working with React and Node.js and I love learning as much as I can about it.</p>
+                </div>
+                <div>
+                    <h4>Easy to work with</h4>
+                    <p>I love working with people and collaborating with other developers to come up with interesing ideas.  I also make sure to have frequent contact with clients in order to provide them with updates and to give them the results they expect.</p>
+                </div>
+                <div>
+                    <h4>Always learning</h4>
+                    <p>I am very passionate about what I do, and I am always learning new and better ways to build applications.  I love to learn from other developers and I am constantly making myself better to produce the best results.</p>
+                </div>
             </div>
-            <div>
-                <h4>Focus on JavaScript</h4>
-                <p>Over the past 4 years I've been working with JavaScript to create beautiful websites and projects for myself and for clients.  I love working with React and Node.js and I love learning as much as I can about it.</p>
-            </div>
-            <div>
-                <h4>Easy to work with</h4>
-                <p>I love working with people and collaborating with other developers to come up with interesing ideas.  I also make sure to have frequent contact with clients in order to provide them with updates and to give them the results they expect.</p>
-            </div>
-            <div>
-                <h4>Always learning</h4>
-                <p>I am very passionate about what I do, and I am always learning new and better ways to build applications.  I love to learn from other developers and I am constantly making myself better to produce the best results.</p>
-            </div>
-        </div>
 
-        <div className="skill-cards">
-            <Card style={{ width: '18rem' }}>
-                <i style={{ fontSize: '40px', textAlign: 'center', padding: '5%', color: 'rgba(10, 5, 92, 0.781)'}}class="fas fa-desktop"></i>
-                <Card.Body>
-                    <Card.Title>Front End</Card.Title>
-                    <ListGroup variant="flush">
-                        <ListGroup.Item>HTML</ListGroup.Item>
-                        <ListGroup.Item>CSS</ListGroup.Item>
-                        <ListGroup.Item>Bootstrap</ListGroup.Item>
-                        <ListGroup.Item>Material UI</ListGroup.Item>
-                        <ListGroup.Item>JavaScript</ListGroup.Item>
-                        <ListGroup.Item>ReactJS | Redux</ListGroup.Item>
-                    </ListGroup>
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '18rem' }}>
-                <i style={{ fontSize: '40px', textAlign: 'center', padding: '5%', color: 'rgba(10, 5, 92, 0.781)'}} class="fas fa-server"></i>
-                <Card.Body>
-                    <Card.Title>BackEnd</Card.Title>
-                    <ListGroup variant="flush">
-                        <ListGroup.Item>Node.JS</ListGroup.Item>
-                        <ListGroup.Item>Express</ListGroup.Item>
-                        <ListGroup.Item>MongoDB</ListGroup.Item>
-                        <ListGroup.Item>PostgreSQL</ListGroup.Item>
-                        <ListGroup.Item>Ruby</ListGroup.Item>
-                    </ListGroup>
-                </Card.Body>
-            </Card>
-        </div>
+            <div className="skill-cards">
+                <Card style={{ width: '18rem' }}>
+                    <i style={{ fontSize: '40px', textAlign: 'center', padding: '5%', color: 'rgba(10, 5, 92, 0.781)'}}class="fas fa-desktop"></i>
+                    <Card.Body>
+                        <Card.Title>Front End</Card.Title>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>HTML</ListGroup.Item>
+                            <ListGroup.Item>CSS</ListGroup.Item>
+                            <ListGroup.Item>Bootstrap</ListGroup.Item>
+                            <ListGroup.Item>Material UI</ListGroup.Item>
+                            <ListGroup.Item>JavaScript</ListGroup.Item>
+                            <ListGroup.Item>ReactJS | Redux</ListGroup.Item>
+                        </ListGroup>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem' }}>
+                    <i style={{ fontSize: '40px', textAlign: 'center', padding: '5%', color: 'rgba(10, 5, 92, 0.781)'}} class="fas fa-server"></i>
+                    <Card.Body>
+                        <Card.Title>BackEnd</Card.Title>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>Node.JS</ListGroup.Item>
+                            <ListGroup.Item>Express</ListGroup.Item>
+                            <ListGroup.Item>MongoDB</ListGroup.Item>
+                            <ListGroup.Item>PostgreSQL</ListGroup.Item>
+                            <ListGroup.Item>Ruby</ListGroup.Item>
+                        </ListGroup>
+                    </Card.Body>
+                </Card>
+            </div>
 
        
-    </div>
+    </>
 );
 
 
